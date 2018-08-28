9652100f5e70a5ce812c3c4e51543e9fd57825f1
Akanksha <agaur05@gmail.com>
2018-08-28 13:30:25 -0500

    adding div id to use for choices

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -1,7 +1,25 @@
 
 //declare and initialize array
-var languages = ['PYTHON', 'JAVASCRIPT', 'PERL', 'JAVA', 'ANGULARJS', 'PHP', 'BASIC', 'NODEJS', 'RUBY'];
-var computerChoice = languages[Math.floor(Math.random() * languages.length)];
+var choices =[]; 
+//to select id from first DIV and display choices according to DIV ID. 
+var x= document.getElementsByTagName("DIV")[0];
+console.log(x.id);
+if(x.id === "languages"){
+    choices=['PYTHON', 'JAVASCRIPT', 'PERL', 'JAVA', 'ANGULARJS', 'PHP', 'BASIC', 'NODEJS', 'RUBY'];
+}else if(x.id === "gameOfThrones"){
+    choices= ['BOLTON','BARATHEON','GREYJOY','LANNISTER','MARTELL','STARK','TARGARYEN','TYRELL'];
+}else if(x.id === "football"){
+    choices= ["EVERTON", "LIVERPOOL", "SWANSEA", "CHELSEA", "HULL", "MANCHESTER", "NEWCASTLE"];
+}else if(x.id === "cities"){
+    choices=['HOUSTON','AUSTIN','CHICAGO','DALLAS','MIAMI','ATLANTA','DENVER','SEATTLE'];
+}else if(x.id === "animals"){
+    choices=['DOG','PANDA','ELEPHANT','RHINO','ZEBRA','GIRAFFE','DEER','BEAR'];
+}else{
+    choices=['AVATAR','TITANIC','SUPERMAN','ALIEN','JAWS','JUMANJI','SPIDERMAN','CONJURING'];
+}
+
+//setting rest of the variables based on array chosen
+var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 var myLength = computerChoice.length;
 var win = myLength;
 var letters = computerChoice.split('');
@@ -11,6 +29,8 @@ var userLetter = "";
 var display = [myLength];
 var guessedLetters = [];
 
+
+//setup the variables
 var setup = function () {
     for (var i = 0; i < myLength; i++) {
         display[i] = "_ ";
@@ -22,6 +42,7 @@ var setup = function () {
 }
 
 
+//when user presses the key
 document.onkeyup = function (event) {
     var userText = event.key;
     output = "";
@@ -55,12 +76,16 @@ document.onkeyup = function (event) {
         }
         if (win === 0) {
             document.getElementById("attempts").innerHTML = "You Win!!Guess Next..."
-            location.reload();
+             // 3 seconds timer before page refreshes
+            setTimeout("location.reload(true);", 3000);
+            // location.reload();
         } 
         else if (attemptsLeft < 1 && win > 0) {
             console.log("not in win:" +win);
             document.getElementById("attempts").innerHTML = "You Lose!!" + " The correct answer is: "+ computerChoice + ". Let's try again!!" ;   
-            location.reload();
+            // 3 seconds timer before page refreshes
+            setTimeout("location.reload(true);", 3000);
+            // location.reload();
         } else {
             document.getElementById("attempts").innerHTML = "You have " + attemptsLeft + " chances left."
         }
